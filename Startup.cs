a936d7ee72ad14e02b030463f74536f7f8c7c3bc
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-05-07 20:55:18 +0300

    used sqlLite for dev and sqlServer fro other

--- Startup.cs
+++ Startup.cs
@@ -15,20 +15,29 @@ namespace writeAJoke
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment {get;}
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-
-            services.AddDbContext<writeAJokeContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("JokeContext")));
+            if(Environment.IsDevelopment())
+            {
+                services.AddDbContext<writeAJokeContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("JokeContext")));
+            }
+            else
+            {
+                services.AddDbContext<writeAJokeContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("JokeContext")));
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
