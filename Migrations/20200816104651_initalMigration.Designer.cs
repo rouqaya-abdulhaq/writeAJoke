760d286b5344cbec815f0b0aa336fce395923a12
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-08-16 13:48:59 +0300

    update database to identity

--- Migrations/20200816104651_initalMigration.Designer.cs
+++ Migrations/20200816104651_initalMigration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using writeAJoke.Data;
 
 namespace writeAJoke.Migrations
 {
     [DbContext(typeof(writeAJokeContext))]
-    partial class writeAJokeContextModelSnapshot : ModelSnapshot
+    [Migration("20200816104651_initalMigration")]
+    partial class initalMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
