30a373b60038fe43367301476318d674c3885797
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-08-17 17:16:56 +0300

    added token and cookies middleware to the pipeline

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,14 @@ namespace writeAJoke
             }
 
             services.AddIdentity<IdentityUser, IdentityRole>()
-                    .AddEntityFrameworkStores<writeAJokeContext>();
+                    .AddEntityFrameworkStores<writeAJokeContext>()
+                    .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie( options => {
+                options.LoginPath = "/Login";
+                options.LogoutPath = "/Logout";
+                options.AccessDeniedPath = "/AccessDenied";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
